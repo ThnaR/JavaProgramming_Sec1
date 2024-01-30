eca819ef5f4937ca1f7d2906cd4bee7faa4e4ac4
ThnaR <thanutchai2548@gmail.com>
2024-01-30 11:39:56 +0700

    Labsheet9

--- JavaProject_Lab9/src/AuthorDemo2.java
+++ JavaProject_Lab9/src/AuthorDemo2.java
@@ -6,7 +6,7 @@ public class AuthorDemo2 {
 		Scanner scan = new Scanner(System.in);
 		String[] author = new String[4]; 
 		for(int i =1;i<author.length;i++) {
-		System.out.println("Information Author "+ i);
+		System.out.println("Information Author "+ i );
 		System.out.print("Input author name   : ");
 		name = scan.nextLine();
 		System.out.print("Input author e-mail : ");
